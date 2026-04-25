feaeda68a21b0fa1fed6815d38c6e7c048f38cbe
charankadaveru-cyber <charankadaveru@gmail.com>
2026-04-25 16:13:48 +0530

    fixed server

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://capd-system-1.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
     const userInfo = localStorage.getItem("userInfo");
+
     if (userInfo) {
         const token = JSON.parse(userInfo).token;
         config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
 });
 
